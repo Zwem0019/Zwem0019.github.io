2713b6009c7ace4d90899abed42a250006ba7707
Frans Zwemer <zwem0019@hz.nl>
2022-10-10 12:28:58 +0200

    js added

---
+++ js/dashboard.js
@@ -0,0 +1,8 @@
+const Y1Q1PCO = document.getElementById('Y1Q1-PCO');
+Y1Q1PCO.textContent = 'Unknown';
+
+const Y1Q1CSB = document.getElementById('Y1Q1-CSB');
+Y1Q1CSB.textContent = 'Unknown';
+
+const Y1Q1PB = document.getElementById('Y1Q1-PB');
+Y1Q1PB.textContent = 'Unknown';
