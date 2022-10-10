c32f707e36cd420110f99cdd8175ba688bfa5dd5
Frans Zwemer <zwem0019@hz.nl>
2022-10-10 13:44:01 +0200

    function added

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,8 +1,13 @@
 const Y1Q1PCO = document.getElementById('Y1Q1-PCO');
-Y1Q1PCO.textContent = 'Unknown';
 
-const Y1Q1CSB = document.getElementById('Y1Q1-CSB');
-Y1Q1CSB.textContent = 'Unknown';
+function gradeassignment (grade, element) {
+  element.textContent = grade;
+  element.classList.add('centertext');
+  if (grade >= 5.5) {
+    element.classList.add('passed');
+  } else {
+    element.classList.add('failed');
+  }
+}
 
-const Y1Q1PB = document.getElementById('Y1Q1-PB');
-Y1Q1PB.textContent = 'Unknown';
+gradeassignment(Y1Q1PCO);
