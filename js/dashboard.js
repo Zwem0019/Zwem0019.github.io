a4b57b602a8167ddd5e92eacf37d190706aae9fa
Frans Zwemer <zwem0019@hz.nl>
2022-10-11 15:22:11 +0200

    javascript changes

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,13 +1,23 @@
-const Y1Q1PCO = document.getElementById('Y1Q1-PCO');
+const gradetable = document.getElementById('gradetable');
+const grades = gradetable.querySelectorAll('.grade');
+for (let i = 0; i < grades.length; i++) {
+  console.log(grades[i]);
+}
 
+/**
+ * @param {number} grade Grade of the exam
+ * @param {*} element id of the element in the table
+ */
 function gradeassignment (grade, element) {
   element.textContent = grade;
   element.classList.add('centertext');
-  if (grade >= 5.5) {
+  if (grade >= 5.5 && grade <= 10) {
     element.classList.add('passed');
-  } else {
+  } else if (grade >= 0 && grade < 5.5) {
     element.classList.add('failed');
+  } else {
+    element.textContent = 'Invalid';
   }
 }
 
-gradeassignment(Y1Q1PCO);
+gradeassignment(grades[0]);
