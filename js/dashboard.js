6af04ce1ed667a12c0ae0cc75fff245e53eaa5fd
Frans Zwemer <zwem0019@hz.nl>
2022-10-25 13:45:22 +0200

    table made in js

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,31 +1,22 @@
-const gradetable = document.getElementById('gradetable');
-const grades = gradetable.querySelectorAll('.grade');
-
-/**
- * @param {number} grade Grade of the exam
- * @param {*} element id of the element in the table
- */
-function gradeassignment (grade, element) {
-  element.textContent = grade;
-  element.classList.add('centertext');
-  if (grade >= 5.5 && grade <= 10) {
-    element.classList.add('passed');
-  } else if (grade >= 0 && grade < 5.5) {
-    element.classList.add('failed');
-  } else {
-    element.textContent = 'Invalid';
-  }
-}
-
-gradeassignment(9.8, grades[0]);
-
 const studymonitor = [
   {
     name: '1',
     courses: [
-      { name: 'Program and Career Orientation', ec: 2.5, exam: { grade: 9.8, type: 'Assesment exam' } },
-      { name: 'Computer Science Basics', ec: 5, exam: { grade: 'Unknown', type: 'Written exam' } },
-      { name: 'Programming Basics', ec: 5, exam: { grade: 'Unknown', type: 'Case study exam' } }
+      {
+        name: 'Program and Career Orientation',
+        ec: 2.5,
+        exam: [{ grade: 9.8, type: 'Assesment exam' }]
+      },
+      {
+        name: 'Computer Science Basics',
+        ec: 5,
+        exam: [{ grade: 'Unknown', type: 'Written exam' }]
+      },
+      {
+        name: 'Programming Basics',
+        ec: 5,
+        exam: [{ grade: 'Unknown', type: 'Case study exam' }]
+      }
     ]
 
   },
@@ -33,7 +24,13 @@ const studymonitor = [
   {
     name: '2',
     courses: [
-      { name: 'Object Oriented Programming', ec: 10, exam: [{ grade: 'Unknown', type: 'Case study (50%)', percentage: 0.5 }, { grade: 'Unknown', type: 'Project (50%)', percentage: 0.5 }] }
+      {
+        name: 'Object Oriented Programming',
+        ec: 10,
+        exam: [
+          { grade: 'Unknown', type: 'Case study (50%)', percentage: 0.5 },
+          { grade: 'Unknown', type: 'Project (50%)', percentage: 0.5 }]
+      }
     ]
 
   },
@@ -41,8 +38,19 @@ const studymonitor = [
   {
     name: '3',
     courses: [
-      { name: 'Framework Development 1', ec: 5, exam: { grade: 'Unknown', type: 'Case study' } },
-      { name: 'Framework Project 1', ec: 7.5, exam: [{ grade: 'Unknown', type: 'Project (33%)', percentage: (1 / 3) }, { grade: 'Unknown', type: 'Case study exam (33%)', percentage: (1 / 3) }, { grade: 'Unknown', type: 'Report (33%)', percentage: (1 / 3) }] }
+      {
+        name: 'Framework Development 1',
+        ec: 5,
+        exam: [{ grade: 'Unknown', type: 'Case study' }]
+      },
+      {
+        name: 'Framework Project 1',
+        ec: 7.5,
+        exam: [
+          { grade: 'Unknown', type: 'Project (33%)', percentage: (1 / 3) },
+          { grade: 'Unknown', type: 'Case study exam (33%)', percentage: (1 / 3) },
+          { grade: 'Unknown', type: 'Report (33%)', percentage: (1 / 3) }]
+      }
     ]
 
   },
@@ -50,7 +58,15 @@ const studymonitor = [
   {
     name: '4',
     courses: [
-      { name: 'Framework Development 2', ec: 10, exam: [{ grade: 'Unknown', type: 'Portfolio (50%)', percentage: 0.5 }, { grade: 'Unknown', type: 'Project (25%)', percentage: 0.25 }, { grade: 'Unknown', type: 'Assesment (25%)', percentage: 0.25 }] }
+      {
+        name: 'Framework Development 2',
+        ec: 10,
+        exam: [
+          { grade: 'Unknown', type: 'Portfolio (50%)', percentage: 0.5 },
+          { grade: 'Unknown', type: 'Project (25%)', percentage: 0.25 },
+          { grade: 'Unknown', type: 'Assesment (25%)', percentage: 0.25 }
+        ]
+      }
     ]
 
   },
@@ -58,7 +74,11 @@ const studymonitor = [
   {
     name: 'Year',
     courses: [
-      { name: 'Personal Professional Development', ec: 12.5, exam: { grade: 'Unknown', type: 'Portfolio' } }
+      {
+        name: 'Personal Professional Development',
+        ec: 12.5,
+        exam: [{ grade: 'Unknown', type: 'Portfolio' }]
+      }
     ]
 
   },
@@ -66,108 +86,156 @@ const studymonitor = [
   {
     name: 'Whenever',
     courses: [
-      { name: 'Personality 1', ec: 1.25, exam: { grade: 'Unknown', type: 'Portfolio' } },
-      { name: 'Personality 2', ec: 1.25, exam: { grade: 'Unknown', type: 'Portfolio' } }
+      {
+        name: 'Personality 1',
+        ec: 1.25,
+        exam: [{ grade: 'Unknown', type: 'Portfolio' }]
+      },
+      {
+        name: 'Personality 2',
+        ec: 1.25,
+        exam: [{ grade: 'Unknown', type: 'Portfolio' }]
+      }
     ]
 
   }
 ];
 
-console.log(studymonitor[0].courses[0].exam.grade);
+function tableRender () {
+  const table = document.getElementById('gradetable');
+  table.classList.add('centertext');
+  const headers = ['Quartile', 'Course', 'Ec', 'Exam', 'Grade'];
+  for (let i = 0; i < headers.length; i++) {
+    const th = document.createElement('th');
+    th.textContent = headers[i];
+    table.appendChild(th);
+  }
+  for (let i = 0; i < studymonitor.length; i++) {
+    const tr = document.createElement('tr');
+    const td1 = document.createElement('td');
+    td1.textContent = studymonitor[i].name;
 
-const table = document.getElementById('gradetable');
-table.classList.add('centertext');
-table.innerHTML = `
-<tr>
-  <th>Quartile</th>
-  <th>Course</th>
-  <th>Ec</th>
-  <th>Exam</th>
-  <th>Grade</th>
-</tr>
-<tr>
-  <td rowspan="3">${studymonitor[0].name}</td>
-  <td>${studymonitor[0].courses[0].name}</td>
-  <td class="ec">${studymonitor[0].courses[0].ec}</td>
-  <td>${studymonitor[0].courses[0].exam.type}</td>
-  <td class="grade">${studymonitor[0].courses[0].exam.grade}</td>
-</tr>
-<tr>
-<td>${studymonitor[0].courses[1].name}</td>
-<td class="ec">${studymonitor[0].courses[1].ec}</td>
-<td>${studymonitor[0].courses[1].exam.type}</td>
-<td class="grade">${studymonitor[0].courses[1].exam.grade}</td>
-</tr>
-<tr>
-<td>${studymonitor[0].courses[2].name}</td>
-<td class="ec">${studymonitor[0].courses[2].ec}</td>
-<td>${studymonitor[0].courses[2].exam.type}</td>
-<td class="grade">${studymonitor[0].courses[2].exam.grade}</td>
-</tr>
-<tr>
-  <td rowspan="2">${studymonitor[1].name}</td>
-  <td rowspan="2">${studymonitor[1].courses[0].name}</td>
-  <td class="ec"rowspan="2">${studymonitor[1].courses[0].ec}</td>
-  <td>${studymonitor[1].courses[0].exam[0].type}</td>
-  <td class="grade">${studymonitor[1].courses[0].exam[0].grade}</td>
-</tr>
-<tr>
-  <td>${studymonitor[1].courses[0].exam[1].type}</td>
-  <td class="grade">${studymonitor[1].courses[0].exam[1].grade}</td>
-</tr>
-<tr>
-  <td rowspan="4">${studymonitor[2].name}</td>
-  <td>${studymonitor[2].courses[0].name}</td>
-  <td class="ec">${studymonitor[2].courses[0].ec}</td>
-  <td>${studymonitor[2].courses[0].exam.type}</td>
-  <td class="grade">${studymonitor[2].courses[0].exam.grade}</td>
-</tr>
-<tr>
-  <td rowspan="3">${studymonitor[2].courses[1].name}</td>
-  <td class="ec"rowspan="3">${studymonitor[2].courses[1].ec}</td>
-  <td>${studymonitor[2].courses[1].exam[0].type}</td>
-  <td class="grade">${studymonitor[2].courses[1].exam[0].grade}</td>
-</tr>
-<tr>
-  <td>${studymonitor[2].courses[1].exam[1].type}</td>
-  <td class="grade">${studymonitor[2].courses[1].exam[1].grade}</td>
-</tr>
-<tr>
-  <td>${studymonitor[2].courses[1].exam[2].type}</td>
-  <td class="grade">${studymonitor[2].courses[1].exam[2].grade}</td>
-</tr>
-<tr>
-  <td rowspan="3">${studymonitor[3].name}</td>
-  <td rowspan="3">${studymonitor[3].courses[0].name}</td>
-  <td class="ec"rowspan="3">${studymonitor[3].courses[0].ec}</td>
-  <td>${studymonitor[3].courses[0].exam[0].type}</td>
-  <td class="grade">${studymonitor[3].courses[0].exam[0].grade}</td>
-</tr>
-<tr>
-  <td>${studymonitor[3].courses[0].exam[1].type}</td>
-  <td class="grade">${studymonitor[3].courses[0].exam[1].grade}</td>
-</tr>
-<tr>
-  <td>${studymonitor[3].courses[0].exam[2].type}</td>
-  <td class="grade">${studymonitor[3].courses[0].exam[2].grade}</td>
-</tr>
-<tr>
-  <td>${studymonitor[4].name}</td>
-  <td>${studymonitor[4].courses[0].name}</td>
-  <td class="ec">${studymonitor[4].courses[0].ec}</td>
-  <td>${studymonitor[4].courses[0].exam.type}</td>
-  <td class="grade">${studymonitor[4].courses[0].exam.grade}</td>
-</tr>
-<tr>
-  <td rowspan="2">${studymonitor[5].name}</td>
-  <td>${studymonitor[5].courses[0].name}</td>
-  <td class="ec">${studymonitor[5].courses[0].ec}</td>
-  <td>${studymonitor[5].courses[0].exam.type}</td>
-  <td class="grade">${studymonitor[5].courses[0].exam.grade}</td>
-</tr>
-<tr>
-  <td>${studymonitor[5].courses[1].name}</td>
-  <td class="ec">${studymonitor[5].courses[1].ec}</td>
-  <td>${studymonitor[5].courses[1].exam.type}</td>
-  <td class="grade">${studymonitor[5].courses[1].exam.grade}</td>
-</tr>`;
+    for (let j = 0; j < studymonitor[i].courses.length; j++) {
+      if (j === 0) {
+        let rowspan = 0;
+        for (let m = 0; m < studymonitor[i].courses.length; m++) {
+          for (let l = 0; l < studymonitor[i].courses[m].exam.length; l++) {
+            rowspan++;
+          }
+        }
+        td1.setAttribute('rowspan', rowspan);
+        tr.appendChild(td1);
+      }
+      for (let k = 0; k < studymonitor[i].courses[j].exam.length; k++) {
+        if (j > 0) {
+          if (k >= 1) {
+            const tr3 = document.createElement('tr');
+            const td4 = document.createElement('td');
+            td4.textContent = studymonitor[i].courses[j].exam[k].type;
+            const td5 = document.createElement('td');
+            td5.textContent = studymonitor[i].courses[j].exam[k].grade;
+            td5.classList.add('grade');
+            td5.classList.add('grade');
+            tr3.appendChild(td4);
+            tr3.appendChild(td5);
+            table.appendChild(tr3);
+          } else {
+            const tr2 = document.createElement('tr');
+            const td2 = document.createElement('td');
+            td2.textContent = studymonitor[i].courses[j].name;
+            const td3 = document.createElement('td');
+            td3.textContent = studymonitor[i].courses[j].ec;
+            if (studymonitor[i].courses[j].exam.length >= 1) {
+              td2.setAttribute('rowspan', studymonitor[i].courses[j].exam.length);
+              td3.setAttribute('rowspan', studymonitor[i].courses[j].exam.length);
+            }
+            const td4 = document.createElement('td');
+            td4.textContent = studymonitor[i].courses[j].exam[k].type;
+            const td5 = document.createElement('td');
+            td5.textContent = studymonitor[i].courses[j].exam[k].grade;
+            td5.classList.add('grade');
+            tr2.appendChild(td2);
+            tr2.appendChild(td3);
+            tr2.appendChild(td4);
+            tr2.appendChild(td5);
+            table.appendChild(tr2);
+          }
+        } else {
+          if (k >= 1) {
+            const tr3 = document.createElement('tr');
+            const td4 = document.createElement('td');
+            td4.textContent = studymonitor[i].courses[j].exam[k].type;
+            const td5 = document.createElement('td');
+            td5.textContent = studymonitor[i].courses[j].exam[k].grade;
+            td5.classList.add('grade');
+            tr3.appendChild(td4);
+            tr3.appendChild(td5);
+            table.appendChild(tr3);
+          } else {
+            const td2 = document.createElement('td');
+            td2.textContent = studymonitor[i].courses[j].name;
+            const td3 = document.createElement('td');
+            td3.textContent = studymonitor[i].courses[j].ec;
+            if (studymonitor[i].courses[j].exam.length >= 1) {
+              td2.setAttribute('rowspan', studymonitor[i].courses[j].exam.length);
+              td3.setAttribute('rowspan', studymonitor[i].courses[j].exam.length);
+            }
+            const td4 = document.createElement('td');
+            td4.textContent = studymonitor[i].courses[j].exam[k].type;
+            const td5 = document.createElement('td');
+            td5.textContent = studymonitor[i].courses[j].exam[k].grade;
+            td5.classList.add('grade');
+            tr.appendChild(td2);
+            tr.appendChild(td3);
+            tr.appendChild(td4);
+            tr.appendChild(td5);
+            table.appendChild(tr);
+          }
+        }
+      }
+    }
+  }
+  console.log(table);
+}
+tableRender();
+
+function calculateEc () {
+  let ec = 0;
+  let coursesuccess = 0;
+  for (let i = 0; i < studymonitor.length; i++) {
+    for (let j = 0; j < studymonitor[i].courses.length; j++) {
+      for (let k = 0; k < studymonitor[i].courses[j].exam.length; k++) {
+        if (studymonitor[i].courses[j].exam[k].grade >= 5.5) {
+          coursesuccess++;
+          if (coursesuccess === studymonitor[i].courses[j].exam.length) {
+            ec += studymonitor[i].courses[j].ec;
+          }
+        }
+      }
+      coursesuccess = 0;
+    }
+  }
+  progressbar(ec);
+  console.log(ec);
+}
+calculateEc();
+
+function progressbar (ec) {
+  const nbsa = document.getElementById('NBSA');
+  nbsa.textContent = `NBSA: ${ec}/45`;
+
+  const css = document.querySelector(':root');
+  const progressy1 = ec / 60 * 100;
+  const progressoveral = ec / 240 * 100;
+  if (progressy1 >= 100) {
+    css.style.setProperty('--progressy1color', 'green');
+  } else if (progressy1 >= 90) {
+    css.style.setProperty('--progressy1color', 'blue');
+  } else if (progressy1 >= 75) {
+    css.style.setProperty('--progressy1color', 'yellow');
+  } else {
+    css.style.setProperty('--progressy1color', 'red');
+  }
+  css.style.setProperty('--progressy1', progressy1 + '%');
+  css.style.setProperty('--progressoveral', progressoveral + '%');
+}
